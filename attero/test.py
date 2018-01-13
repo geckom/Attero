8d47960a41aaa2727a9404a288c6d8276c2d6d20
Gecko <geckom@redteamr.com>
2018-01-13 14:45:35 +1000

    Added tasks unit test

--- attero/test.py
+++ attero/test.py
@@ -42,3 +42,31 @@ class ProjectListTests(TestCase):
         # Check for no projects text
         self.assertContains(resp, "No projects are available.")
 
+
+
+class AllTaskListTests(TestCase):
+    def test_no_projects(self):
+        """
+        If no tasks exist, an appropriate message is displayed.
+        """
+        test_user1 = User.objects.create_user(username='testuser1', password='12345')
+        test_user1.save()
+
+        login = self.client.login(username='testuser1', password='12345')
+        resp = self.client.get(reverse('task-list'))
+
+        #Check our user is logged in
+        self.assertEqual(str(resp.context['user']), 'testuser1')
+        #Check that we got a response "success"
+        self.assertEqual(resp.status_code, 200)
+
+        #Check we used correct template
+        self.assertTemplateUsed(resp, 'task/all.html')
+
+        #Check that initially we don't have any tasks in list 
+        self.assertTrue('tasks' in resp.context)
+        self.assertEqual( len(resp.context['tasks']),0)
+
+        # Check for no tasks text
+        self.assertContains(resp, "No tasks are available.")
+
